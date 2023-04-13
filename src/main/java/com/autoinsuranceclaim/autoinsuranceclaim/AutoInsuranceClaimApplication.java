ce6880701e44226ecc5e5e587167520b1c67cca8
ZGhali <ZGhali@win.cccis.com>
2023-04-13 16:47:43 -0500

    Initial Commit

---
+++ src/main/java/com/autoinsuranceclaim/autoinsuranceclaim/AutoInsuranceClaimApplication.java
@@ -0,0 +1,13 @@
+package com.autoinsuranceclaim.autoinsuranceclaim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoInsuranceClaimApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutoInsuranceClaimApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/autoinsuranceclaim/autoinsuranceclaim/AutoInsuranceClaimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.autoinsuranceclaim.autoinsuranceclaim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoInsuranceClaimApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
